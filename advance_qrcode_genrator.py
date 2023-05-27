d38519781ba55904416e1b0461ff4170bbb12401
rohitkumarmehta18 <134787619+rohitkumarmehta18@users.noreply.github.com>
2023-05-27 19:13:44 +0530

    Update advance_qrcode_genrator.py

--- advance_qrcode_genrator.py
+++ advance_qrcode_genrator.py
@@ -6,8 +6,8 @@ qr=qrcode.QRCode(version=1,
                  error_correction=qrcode.constants.ERROR_CORRECT_H,
                  box_size=10,
                  border=4,)
-qr.add_data("https://www.youtube.com/@sikhekuchhnaya")
+qr.add_data("https://www.youtube.com/") #paste your text or link here
 qr.make(fit=True)
 img= qr.make_image(fill_color="blue",
                    back_color="white")
-img.save("sikhe_kuch_naya.png")
+img.save("image_name.png")
